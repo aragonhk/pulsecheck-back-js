68d5aa3a1f8bd1e2744559df5f747f36c7ddbd12
aragonhk <michael.c.ng@live.com>
2017-09-01 09:59:53 -0700

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
